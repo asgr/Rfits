f98d7a978bccf3ec654409a07dbac09c61985c0b
Aaron Robotham <aaron.robotham@uwa.edu.au>
2022-05-24 14:36:46 +0200

    Stricter gunzip

--- R/Rfits_gunzip.R
+++ R/Rfits_gunzip.R
@@ -2,17 +2,17 @@ Rfits_gunzip = function(filename, tempdir=NULL){
   assertCharacter(filename, max.len=1)
   filename = path.expand(filename)
   assertAccess(filename, access='r')
-  if(grepl('fits.gz',filename,fixed=TRUE) | grepl('fit.gz',filename,fixed=TRUE)){
+  if(grepl('fits.gz$',filename,fixed=TRUE) | grepl('fit.gz$',filename,fixed=TRUE)){
     if(filename %in% options()$Rfits_gunzip[,1]){
       filename = options()$Rfits_gunzip[options()$Rfits_gunzip[,1] == filename,2]
     }else{
-      if(!requireNamespace("R.utils", quietly = TRUE)){
+      if(!requireNamespace("R.utils", quietly=TRUE)){
         stop('The R.utils package is needed to decompress the target fits.gz. Please install it from CRAN', call. = FALSE)
       }
       if(is.null(tempdir)){
         tempdir = tempdir()
       }
-      file_temp = paste(tempdir,strsplit(basename(filename),'.gz')[[1]], sep='/')
+      file_temp = paste(tempdir,strsplit(basename(filename),'\\.gz$')[[1]], sep='/')
       R.utils::gunzip(filename, destname=file_temp, remove=FALSE, overwrite=TRUE)
       options(Rfits_gunzip = rbind(options()$Rfits_gunzip, c(filename, file_temp)))
       return(file_temp)
