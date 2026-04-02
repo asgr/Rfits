ec8b2fd1029fad83a87b8c3b654037f044df22a4
Aaron Robotham <aaron.robotham@uwa.edu.au>
2026-04-02 12:02:05 +0800

    Convert to just a warning

--- src/Rfits.cpp
+++ src/Rfits.cpp
@@ -199,7 +199,6 @@ SEXP Cfits_read_col(Rcpp::String filename, int colref=1, int ext=2,
   
   if (startrow + nrow - 1 > nrow_total) {
     Rcpp::warning("Requested range exceeds number of rows in table");
-    nrow = nrow_total - startrow + 1;
   }
   
   if ( typecode == TSTRING ) {
